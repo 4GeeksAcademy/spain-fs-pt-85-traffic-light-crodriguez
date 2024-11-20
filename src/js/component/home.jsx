7ab72385f7dd36bd79c7ec3bb6cf2004677c3d91
CRVanware <crodriguez@vanware.io>
2024-11-20 20:13:13 +0000

    versión 1.0

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
+	const [rojo,setRojo] = useState("redlight")
+	const [amarillo,setAmarillo] = useState("yellowlight")
+	const [verde,setVerde] = useState("greenlight")
+
+	function iluminar(e) {
+		console.log(e.target.className)
+		if (e.target.className === "redlight") {
+			setRojo("redlight brillo")
+			setAmarillo("yellowlight")
+			setVerde("greenlight")
+		} else if (e.target.className === "yellowlight") {
+			setAmarillo("yellowlight brillo")
+			setRojo("redlight")
+			setVerde("greenlight")
+		} else { 
+			setVerde("greenlight brillo")
+			setAmarillo("yellowlight")
+			setRojo("redlight")
+		}
+					
+	}
+		
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="app">
+			<div>
+				<div className="trafficTop"></div>
+				<div className="container">
+					<div className={rojo} onClick={iluminar}></div>
+					<div className={amarillo} onClick={iluminar}></div>
+					<div className={verde} onClick={iluminar}></div>
+				</div>
+			</div>
 		</div>
 	);
 };
